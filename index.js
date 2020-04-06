8e87fceb51550e5c263e340eab63a193d5be418b
Laura Gómez Cabero <lauragcabero@gmail.com>
2020-04-06 21:52:13 +0200

    primer ejercicio js

---
+++ index.js
@@ -0,0 +1,13 @@
+var nombreYApellidos = 'Laura Gomez Cabero';
+var miEdad = 31;
+var diasDeLaSemana = ['lunes', 'martes', 'miercoles', 'jueves', 'viernes','sabado', 'domingo'];
+var valorGravedad = 9.807;
+var tresPrimerosNumerosImpares = {
+    primerNumero: 1,
+    segundoNumero: 3,
+    tercerNumero: 5
+};
+
+// test:
+console.log(diasDeLaSemana[2])
+console.log(tresPrimerosNumerosImpares.segundoNumero)
